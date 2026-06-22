8f96775bcb524225d32c438e28504c352540bc99
Kenshi Muto <kmuto@kmuto.jp>
2026-06-22 18:07:45 +0900

    update setup

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,17 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 composer install
 
 cat > .env <<EOT
+APP_NAME=Laravel
 APP_ENV=local
 APP_DEBUG=true
-APP_KEY=
+APP_URL=http://localhost
 
-DB_CONNECTION=mysql
-DB_HOST=mysql
-DB_PORT=3306
-DB_DATABASE=flight_db
-DB_USERNAME=user
-DB_PASSWORD=password
+DB_CONNECTION=sqlite
+
+OTEL_PHP_AUTOLOAD_ENABLED=true
+OTEL_SERVICE_NAME=flight-backend
+OTEL_TRACES_EXPORTER=otlp
+#OTEL_LOGS_EXPORTER=none
+OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf
+OTEL_EXPORTER_OTLP_ENDPOINT=http://otel-collector:4318
 EOT
 
 php artisan key:generate
